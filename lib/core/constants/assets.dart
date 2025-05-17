76fae1ce6c211dfc7232103c37b9f34a30435692
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-05-17 20:22:45 +0200

    add assets for project

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,104 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsCoverCover1
+  /// Assets/cover/cover1.png
+  static const String assetsCoverCover1 = "Assets/cover/cover1.png";
+
+  /// Assets for assetsCoverCover2
+  /// Assets/cover/cover2.png
+  static const String assetsCoverCover2 = "Assets/cover/cover2.png";
+
+  /// Assets for assetsCoverCover3
+  /// Assets/cover/cover3.png
+  static const String assetsCoverCover3 = "Assets/cover/cover3.png";
+
+  /// Assets for assetsLogoLogo
+  /// Assets/logo/logo.png
+  static const String assetsLogoLogo = "Assets/logo/logo.png";
+
+  /// Assets for assetsLogoLogoBg
+  /// Assets/logo/logo-bg.svg
+  static const String assetsLogoLogoBg = "Assets/logo/logo-bg.svg";
+
+  /// Assets for assetsProductProduct1
+  /// Assets/product/product1.png
+  static const String assetsProductProduct1 = "Assets/product/product1.png";
+
+  /// Assets for assetsProductProduct2
+  /// Assets/product/product2.png
+  static const String assetsProductProduct2 = "Assets/product/product2.png";
+
+  /// Assets for assetsProductProduct3
+  /// Assets/product/product3.png
+  static const String assetsProductProduct3 = "Assets/product/product3.png";
+
+  /// Assets for assetsProductProduct4
+  /// Assets/product/product4.png
+  static const String assetsProductProduct4 = "Assets/product/product4.png";
+
+  /// Assets for assetsProductProduct5
+  /// Assets/product/product5.png
+  static const String assetsProductProduct5 = "Assets/product/product5.png";
+
+  /// Assets for assetsProductProduct6
+  /// Assets/product/product6.png
+  static const String assetsProductProduct6 = "Assets/product/product6.png";
+
+  /// Assets for assetsSvgsArrow
+  /// Assets/svgs/arrow.svg
+  static const String assetsSvgsArrow = "Assets/svgs/arrow.svg";
+
+  /// Assets for assetsSvgsDelivery
+  /// Assets/svgs/delivery.svg
+  static const String assetsSvgsDelivery = "Assets/svgs/delivery.svg";
+
+  /// Assets for assetsSvgsLine
+  /// Assets/svgs/line.png
+  static const String assetsSvgsLine = "Assets/svgs/line.png";
+
+  /// Assets for assetsSvgsMastercard
+  /// Assets/svgs/Mastercard.svg
+  static const String assetsSvgsMastercard = "Assets/svgs/Mastercard.svg";
+
+  /// Assets for assetsSvgsMenu
+  /// Assets/svgs/Menu.svg
+  static const String assetsSvgsMenu = "Assets/svgs/Menu.svg";
+
+  /// Assets for assetsSvgsMin
+  /// Assets/svgs/min.svg
+  static const String assetsSvgsMin = "Assets/svgs/min.svg";
+
+  /// Assets for assetsSvgsPlus
+  /// Assets/svgs/plus.svg
+  static const String assetsSvgsPlus = "Assets/svgs/plus.svg";
+
+  /// Assets for assetsSvgsPromo
+  /// Assets/svgs/promo.svg
+  static const String assetsSvgsPromo = "Assets/svgs/promo.svg";
+
+  /// Assets for assetsSvgsSearch
+  /// Assets/svgs/Search.svg
+  static const String assetsSvgsSearch = "Assets/svgs/Search.svg";
+
+  /// Assets for assetsSvgsShopping bag
+  /// Assets/svgs/shopping bag.svg
+  static const String assetsSvgsShoppingbag = "Assets/svgs/shopping bag.svg";
+
+  /// Assets for assetsSvgsVisa
+  /// Assets/svgs/visa.svg
+  static const String assetsSvgsVisa = "Assets/svgs/visa.svg";
+
+  /// Assets for assetsTexts10
+  /// Assets/texts/10.svg
+  static const String assetsTexts10 = "Assets/texts/10.svg";
+
+  /// Assets for assetsTextsCollection
+  /// Assets/texts/Collection.svg
+  static const String assetsTextsCollection = "Assets/texts/Collection.svg";
+
+  /// Assets for assetsTextsOctober
+  /// Assets/texts/October.svg
+  static const String assetsTextsOctober = "Assets/texts/October.svg";
+}
