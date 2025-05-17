1b2c7c4b43bbc55b5e54eee99d883927b31ec2c5
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-05-17 23:18:56 +0200

    add products model

---
+++ lib/features/Home/presentation/views/data/models/productModel/products_model.dart
@@ -0,0 +1,50 @@
+import 'package:fashion/core/constants/assets.dart';
+
+class ProductsModel {
+  final String image, descripation, name, price;
+
+  ProductsModel({
+    required this.image,
+    required this.price,
+    required this.descripation,
+    required this.name,
+  });
+  static List<ProductsModel> productList = [
+    ProductsModel(
+      image: Assets.assetsProductProduct6,
+      price: '100 €',
+      descripation: 'reversible angora cardigan',
+      name: 'October',
+    ),
+    ProductsModel(
+      image: Assets.assetsProductProduct1,
+      price: '120 €',
+      descripation: 'reversible angora cardigan',
+      name: 'October',
+    ),
+    ProductsModel(
+      image: Assets.assetsProductProduct5,
+      price: '70 €',
+      descripation: 'reversible angora cardigan',
+      name: 'October',
+    ),
+    ProductsModel(
+      image: Assets.assetsProductProduct4,
+      price: '90 €',
+      descripation: 'reversible angora cardigan',
+      name: 'October',
+    ),
+    ProductsModel(
+      image: Assets.assetsProductProduct2,
+      price: '80 €',
+      descripation: 'reversible angora cardigan',
+      name: 'October',
+    ),
+    ProductsModel(
+      image: Assets.assetsProductProduct3,
+      price: '150 €',
+      descripation: 'reversible angora cardigan',
+      name: 'October',
+    ),
+  ];
+}
