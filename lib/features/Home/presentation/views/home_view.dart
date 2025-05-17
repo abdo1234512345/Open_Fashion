f795c6e3f2abe1a2f9b4f4e23a5c9f93d510925b
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-05-17 23:14:38 +0200

    create homeview

--- lib/features/Home/presentation/views/home_view.dart
+++ lib/features/Home/presentation/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:fashion/core/constants/colors.dart';
+import 'package:fashion/core/widgets/custom_app_bar.dart';
 import 'package:fashion/features/Home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +8,10 @@ class HomeView extends StatelessWidget {
   static const String routeName = 'home_view';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: HomeViewBody());
+    return Scaffold(
+      appBar: CustomAppBar(isblack: true),
+      backgroundColor: AppColors.primaryColor,
+      body: HomeViewBody(),
+    );
   }
 }
